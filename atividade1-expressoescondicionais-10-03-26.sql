e412ee3a8c922ab377ac88574be60b4139d9bb4e
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-03-10 08:16:21 -0300

    Add files via upload

---
+++ atividade1-expressoescondicionais-10-03-26.sql
@@ -0,0 +1,16 @@
+#Questão 1:
+
+select nome, de_ferias, bairro from tabela_de_vendedores where nome = 'Cláudia Morais' and bairro = 'Jardins';
+
+#Resposta da questão 1:
+#Sim, ela está de férias.
+
+#Questão 2:
+
+select * from tabela_de_vendedores where percentual_comissao > '0.1' and de_ferias = 1;
+
+#Resposta da questão 2:
+#A pessoa que possui comissão de 0,10 e está de férias é a Roberta Martins.
+
+#Questão 3:
+

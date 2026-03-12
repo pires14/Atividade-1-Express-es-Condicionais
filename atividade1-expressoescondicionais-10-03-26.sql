8bcb31a4927ec3fe052f03095d16d660f16e8245
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-03-12 09:56:28 -0300

    Add files via upload

--- atividade1-expressoescondicionais-10-03-26.sql
+++ atividade1-expressoescondicionais-10-03-26.sql
@@ -14,3 +14,34 @@ select * from tabela_de_vendedores where percentual_comissao > '0.1' and de_feri
 
 #Questão 3:
 
+select * from notas_fiscais where matricula = 00237 or data_venda = '2015-01-12';
+
+#Resposta da questão 3:
+#Porque o CPF pode se repetir mais de uma vez, sendo uma chave estrangeira,
+#já o número das notas fiscais é único e não se repete.
+
+#Questão 4: 
+
+select MAX(preco) from itens_notas_fiscais;
+
+select codigo_do_produto, quantidade, preco from itens_notas_fiscais 
+where quantidade >= 99 and preco > 44;
+
+select NOME_DO_PRODUTO, codigo_do_produto from 
+tabela_de_produtos where codigo_do_produto = 1022450;
+
+#Resposta da questão 4:
+#O item representado é o "Festival de Sabores - 2 Litros - Açai" 
+#e o seu código de produto é: 1022450.
+
+#Questão 5:
+
+#Quem é o cliente que comprou 84 produtos com o número de código igual a 1101035, 
+#cujo número da nota fiscal foi 102?
+#Quem foi o vendedor ou vendedora responsável e qual o nome do produto e seu sabor? 
+#Gere o Diagrama de Entidade - Relacionamento para melhor compreensão.
+
+select cpf, numero from notas_fiscais where numero = 102;
+select nome from tabela_de_clientes where cpf = 8502682733;
+
+#O cliente é o Valdeci da Silva.
